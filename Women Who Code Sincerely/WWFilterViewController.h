ee1ce4854e00ccba91225ecc9561ad12b1887496
romy ilano <romy.ilano@gmail.com>
2013-04-14 10:18:59 -0700

    preparing aviary

---
+++ Women Who Code Sincerely/WWFilterViewController.h
@@ -0,0 +1,15 @@
+//
+//  WWFilterViewController.h
+//  Women Who Code Sincerely
+//
+//  Created by Romy on 4/14/13.
+//  Copyright (c) 2013 Snowyla. All rights reserved.
+//
+
+#import "WWMakeMineViewController.h"
+
+@interface WWFilterViewController : WWMakeMineViewController
+
+-(IBAction)getPhoto:(UIButton *)button;
+
+@end
---
+++ Women Who Code Sincerely/WWFilterViewController.m
@@ -0,0 +1,43 @@
+//
+//  WWFilterViewController.m
+//  Women Who Code Sincerely
+//
+//  Created by Romy on 4/14/13.
+//  Copyright (c) 2013 Snowyla. All rights reserved.
+//
+
+#import "WWFilterViewController.h"
+
+@interface WWFilterViewController ()
+
+@end
+
+@implementation WWFilterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)getPhoto:(UIButton *)button
+{
+    
+}
+
+@end
